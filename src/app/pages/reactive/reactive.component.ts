e76250658dca45426266aa05c3022ac741540223
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-11-24 18:21:31 -0600

    Reactive forms

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  /* Formbuilder helps to create forms swiftly */
+  constructor(private fb: FormBuilder) {
+    this.initForm();
+   }
 
   ngOnInit(): void {
+  }
+
+  // This mehthod initialize the form
+  initForm(){
+    /* Here is defyning the form */
+    this.form = this.fb.group({
+      // [data, syncronus Validators, asynconus Validator]
+      name:['',[
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ],
+      lastname:['',[
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ],
+      mail:['',[
+          Validators.required,
+          Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+        ]
+      ],
+    });
+  }
+
+  /* This method save data */
+  saveData(){
+    console.log(this.form);
+    
   }
 
 }
